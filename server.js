3543b8a96d24e9329fd1436d864c38b16827eaaf
zaakiraza <raaza.zaakir@gmail.com>
2025-08-05 11:11:21 +0500

    yes

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5003;
-const NODE_ENV = process.env.NODE_ENV;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
-});
-
-
-if (NODE_ENV !== 'production') {
+if(process.env.APP_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
   });
